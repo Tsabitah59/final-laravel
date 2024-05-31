3d1cf71620c6383fc61fd2b892939074f772fad5
Tsabitah59 <tsabitahsafa06@gmail.com>
2024-05-31 08:17:17 +0700

    first commit

---
+++ resources/views/layouts/adminhome.blade.php
@@ -0,0 +1,53 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="shortcut icon" href="./img/fav.png" type="image/x-icon">
+
+    <!-- Fontawesome -->
+    <link rel="stylesheet" href="https://kit-pro.fontawesome.com/releases/v5.12.1/css/pro.min.css">
+
+    <!-- Link CSS -->
+    <link rel="stylesheet" type="text/css" href="{{ asset('admin/css/style.css') }}">
+
+    <!-- Flowbite -->
+    <!-- <link href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.3.0/flowbite.min.css"  rel="stylesheet" /> -->
+
+    <title>Seventeenvis Admin</title>
+</head>
+
+<body class="bg-gray-100">
+
+
+    <!-- start navbar -->
+    @include('layouts.include.admin.navbar')
+    <!-- end navbar -->
+
+
+    <!-- strat wrapper -->
+    <div class="h-screen flex flex-row flex-wrap">
+
+        <!-- start sidebar -->
+        @include('layouts.include.admin.sidebar')
+        <!-- end sidbar -->
+
+        <!-- strat content -->
+        @yield('content')
+        <!-- end content -->
+
+    </div>
+    <!-- end wrapper -->
+
+    <!-- script -->
+    <script src="https://cdn.jsdelivr.net/npm/apexcharts"></script>
+    <script src="{{ asset('admin/js/scripts.js') }}"></script>
+    <!-- end script -->
+
+    <!-- Flowbite -->
+    <!-- <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.3.0/flowbite.min.js"></script> -->
+
+</body>
+
+</html>
