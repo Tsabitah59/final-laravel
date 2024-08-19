385d96998d717f73193f58723f4036c782a118a6
Tsabitah59 <tsabitahsafa06@gmail.com>
2024-08-19 13:11:21 +0700

    nyerahh

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Category;
+use App\Models\Story;
 use Illuminate\Http\Request;
 
 use App\Models\User;
@@ -11,11 +13,14 @@ use Illuminate\Support\Facades\Auth;
 class HomeController extends Controller
 {
     public function index() {
+        $categories = Category::all();
+        $trendingStory  = Story::all();
+
         if(Auth::id()) {
             $usertype = Auth()->user()->usertype;
 
             if($usertype == 'user') {
-                return view('dashboard');
+                return view('dashboard', compact('categories', 'trendingStory'));
             } else if($usertype == 'admin') {
                 return view('admin.admindashboard');
             } else {
--- resources/views/frontend/collections/new-story.blade.php
+++ resources/views/frontend/collections/new-story.blade.php
@@ -7,176 +7,40 @@
         <p class="text-center mt-4 text-white"> Let's read some fairy tale legends from our website, <br> increase your creativity by reading stories from this website.</p>
     </div>
 
-    <section class="ml-[130px]">
+    <!-- NewStory -->
+    <section class="ml-[99px] itemm-center mx-auto">
         <!-- Card Body -->
-        <div class="card-body mx-auto mt-[70px]">
-
+        @if(isset($trendingStory) && count($trendingStory) > 0)
+        <div class="card-body mx-auto mt-[40px]">
             <!-- Card -->
-            <div class="card mx-auto flex gap-[60px]">
+            <div class="card mx-auto flex gap-5 overflow-auto scrollbar-hide">
 
                 <!-- Card Item -->
-                <div class="group card-item shadow-sm shadow-transparent bg-[#FFCA42] hover:bg-[#1B3764] w-[270px] h-[380px] pt-3 rounded-lg pl-3 pr-3 transition-all duration-700">
+                @foreach($trendingStory as $story)
+                <div class="group card-item shadow-sm shadow-transparent bg-white hover:bg-[#1B3764] w-[250px] h-[330px] pt-3 rounded-lg pl-3 pr-3 transition-all duration-700">
                     <a class="card-img">
-                        <img src="" alt="" class="h-[170px] rounded-md bg-[#1B3764] group-hover:bg-[#FFCA42]">
+                        <img src="{{ $story->image }}" alt="" class="w-[223px] h-[156px] mx-auto" style="border-radius: 10px 10px 0 0;">
                     </a>
                     <div class="text-card">
-                        <h3 class="mt-[9px] text-white group-hover:text-white text-[18px] transition-all duration-700" style="font-family: 'Quicksand', sans-serif; font-weight: 600;">Si Kancil Yang Cerdik</h3>
-                        <p class="desc text-[12px] text-white mt-3 transition-all duration-700">Diceritakan bahwa kancil seekor hewan kecil yang hidup di hutan belantara. Ia dikenal memiliki akal yang cerdik. Kecerdikannya membuatnya mampu bertahan hidup di hutan.</p>
+                        <h3 class="mt-[9px] text-[#1B3764]  group-hover:text-white text-[18px] transition-all duration-700" style="font-family: 'Quicksand', sans-serif; font-weight: 600;">{{ $story->name}}</h3>
+                        <p class="desc text-[12px] text-[#1B3764]  group-hover:text-white mt-3 transition-all duration-700">{{ $story->synopsis }}</p>
                     </div>
-                    <div class="card-btn flex gap-[30px]">
+                    <div class="card-btn flex gap-[30px] mt-6 items-center">
                         <div>
-                            <button class="btn mt-5 text-white bg-[#1B3764]  group-hover:bg-[#FFCA42] rounded-full w-[144px] h-[30px] transition-all duration-700">Read More</button>
+                            <a href="{{ url($story->slug) }}" class="btn mt-5 text-white bg-[#1B3764]  group-hover:bg-[#FFCA42] rounded-full px-7 py-2 transition-all duration-700">Read More</a>
                         </div>
-                        <div class="mt-[23px] flex gap-3">
-                            <i class="ri-heart-3-fill text-white  group-hover:text-[#FFCA42] text-xl transition-all duration-700"></i>
-                            <i class="ri-bookmark-fill text-white group-hover:text-[#FFCA42] text-xl transition-all duration-700"></i>
+                        <div class="flex gap-3">
+                            <i class="ri-heart-3-fill text-[#FFCA42]  group-hover:text-white text-xl transition-all duration-700"></i>
+                            <i class="ri-bookmark-fill text-[#FFCA42]  group-hover:text-white text-xl transition-all duration-700"></i>
                         </div>
                     </div>
                 </div>
-                <!-- Card Item -->
-                <div class="group card-item shadow-sm shadow-transparent bg-[#FFCA42] hover:bg-[#1B3764] w-[270px] h-[380px] pt-3 rounded-lg pl-3 pr-3 transition-all duration-700">
-                    <a class="card-img">
-                        <img src="" alt="" class="h-[170px] rounded-md bg-[#1B3764] group-hover:bg-[#FFCA42]">
-                    </a>
-                    <div class="text-card">
-                        <h3 class="mt-[9px] text-white group-hover:text-white text-[18px] transition-all duration-700" style="font-family: 'Quicksand', sans-serif; font-weight: 600;">Si Kancil Yang Cerdik</h3>
-                        <p class="desc text-[12px] text-white mt-3 transition-all duration-700">Diceritakan bahwa kancil seekor hewan kecil yang hidup di hutan belantara. Ia dikenal memiliki akal yang cerdik. Kecerdikannya membuatnya mampu bertahan hidup di hutan.</p>
-                    </div>
-                    <div class="card-btn flex gap-[30px]">
-                        <div>
-                            <button class="btn mt-5 text-white bg-[#1B3764]  group-hover:bg-[#FFCA42] rounded-full w-[144px] h-[30px] transition-all duration-700">Read More</button>
-                        </div>
-                        <div class="mt-[23px] flex gap-3">
-                            <i class="ri-heart-3-fill text-white  group-hover:text-[#FFCA42] text-xl transition-all duration-700"></i>
-                            <i class="ri-bookmark-fill text-white group-hover:text-[#FFCA42] text-xl transition-all duration-700"></i>
-                        </div>
-                    </div>
-                </div>
-                <!-- Card Item -->
-                <div class="group card-item shadow-sm shadow-transparent bg-[#FFCA42] hover:bg-[#1B3764] w-[270px] h-[380px] pt-3 rounded-lg pl-3 pr-3 transition-all duration-700">
-                    <a class="card-img">
-                        <img src="" alt="" class="h-[170px] rounded-md bg-[#1B3764] group-hover:bg-[#FFCA42]">
-                    </a>
-                    <div class="text-card">
-                        <h3 class="mt-[9px] text-white group-hover:text-white text-[18px] transition-all duration-700" style="font-family: 'Quicksand', sans-serif; font-weight: 600;">Si Kancil Yang Cerdik</h3>
-                        <p class="desc text-[12px] text-white mt-3 transition-all duration-700">Diceritakan bahwa kancil seekor hewan kecil yang hidup di hutan belantara. Ia dikenal memiliki akal yang cerdik. Kecerdikannya membuatnya mampu bertahan hidup di hutan.</p>
-                    </div>
-                    <div class="card-btn flex gap-[30px]">
-                        <div>
-                            <button class="btn mt-5 text-white bg-[#1B3764]  group-hover:bg-[#FFCA42] rounded-full w-[144px] h-[30px] transition-all duration-700">Read More</button>
-                        </div>
-                        <div class="mt-[23px] flex gap-3">
-                            <i class="ri-heart-3-fill text-white  group-hover:text-[#FFCA42] text-xl transition-all duration-700"></i>
-                            <i class="ri-bookmark-fill text-white group-hover:text-[#FFCA42] text-xl transition-all duration-700"></i>
-                        </div>
-                    </div>
-                </div>
-                <!-- Card Item -->
-                <div class="group card-item shadow-sm shadow-transparent bg-[#FFCA42] hover:bg-[#1B3764] w-[270px] h-[380px] pt-3 rounded-lg pl-3 pr-3 transition-all duration-700">
-                    <a class="card-img">
-                        <img src="" alt="" class="h-[170px] rounded-md bg-[#1B3764] group-hover:bg-[#FFCA42]">
-                    </a>
-                    <div class="text-card">
-                        <h3 class="mt-[9px] text-white group-hover:text-white text-[18px] transition-all duration-700" style="font-family: 'Quicksand', sans-serif; font-weight: 600;">Si Kancil Yang Cerdik</h3>
-                        <p class="desc text-[12px] text-white mt-3 transition-all duration-700">Diceritakan bahwa kancil seekor hewan kecil yang hidup di hutan belantara. Ia dikenal memiliki akal yang cerdik. Kecerdikannya membuatnya mampu bertahan hidup di hutan.</p>
-                    </div>
-                    <div class="card-btn flex gap-[30px]">
-                        <div>
-                            <button class="btn mt-5 text-white bg-[#1B3764]  group-hover:bg-[#FFCA42] rounded-full w-[144px] h-[30px] transition-all duration-700">Read More</button>
-                        </div>
-                        <div class="mt-[23px] flex gap-3">
-                            <i class="ri-heart-3-fill text-white  group-hover:text-[#FFCA42] text-xl transition-all duration-700"></i>
-                            <i class="ri-bookmark-fill text-white group-hover:text-[#FFCA42] text-xl transition-all duration-700"></i>
-                        </div>
-                    </div>
-                </div>
+                @endforeach
             </div>
-            <!-- End -->
-            <!-- Card -->
-            <div class="card mx-auto flex gap-[60px] mt-[60px]">
-
-                <!-- Card Item -->
-                <div class="group card-item shadow-sm shadow-transparent bg-[#FFCA42] hover:bg-[#1B3764] w-[270px] h-[380px] pt-3 rounded-lg pl-3 pr-3 transition-all duration-700">
-                    <a class="card-img">
-                        <img src="" alt="" class="h-[170px] rounded-md bg-[#1B3764] group-hover:bg-[#FFCA42]">
-                    </a>
-                    <div class="text-card">
-                        <h3 class="mt-[9px] text-white group-hover:text-white text-[18px] transition-all duration-700" style="font-family: 'Quicksand', sans-serif; font-weight: 600;">Si Kancil Yang Cerdik</h3>
-                        <p class="desc text-[12px] text-white mt-3 transition-all duration-700">Diceritakan bahwa kancil seekor hewan kecil yang hidup di hutan belantara. Ia dikenal memiliki akal yang cerdik. Kecerdikannya membuatnya mampu bertahan hidup di hutan.</p>
-                    </div>
-                    <div class="card-btn flex gap-[30px]">
-                        <div>
-                            <button class="btn mt-5 text-white bg-[#1B3764]  group-hover:bg-[#FFCA42] rounded-full w-[144px] h-[30px] transition-all duration-700">Read More</button>
-                        </div>
-                        <div class="mt-[23px] flex gap-3">
-                            <i class="ri-heart-3-fill text-white  group-hover:text-[#FFCA42] text-xl transition-all duration-700"></i>
-                            <i class="ri-bookmark-fill text-white group-hover:text-[#FFCA42] text-xl transition-all duration-700"></i>
-                        </div>
-                    </div>
-                </div>
-                <!-- Card Item -->
-                <div class="group card-item shadow-sm shadow-transparent bg-[#FFCA42] hover:bg-[#1B3764] w-[270px] h-[380px] pt-3 rounded-lg pl-3 pr-3 transition-all duration-700">
-                    <a class="card-img">
-                        <img src="" alt="" class="h-[170px] rounded-md bg-[#1B3764] group-hover:bg-[#FFCA42]">
-                    </a>
-                    <div class="text-card">
-                        <h3 class="mt-[9px] text-white group-hover:text-white text-[18px] transition-all duration-700" style="font-family: 'Quicksand', sans-serif; font-weight: 600;">Si Kancil Yang Cerdik</h3>
-                        <p class="desc text-[12px] text-white mt-3 transition-all duration-700">Diceritakan bahwa kancil seekor hewan kecil yang hidup di hutan belantara. Ia dikenal memiliki akal yang cerdik. Kecerdikannya membuatnya mampu bertahan hidup di hutan.</p>
-                    </div>
-                    <div class="card-btn flex gap-[30px]">
-                        <div>
-                            <button class="btn mt-5 text-white bg-[#1B3764]  group-hover:bg-[#FFCA42] rounded-full w-[144px] h-[30px] transition-all duration-700">Read More</button>
-                        </div>
-                        <div class="mt-[23px] flex gap-3">
-                            <i class="ri-heart-3-fill text-white  group-hover:text-[#FFCA42] text-xl transition-all duration-700"></i>
-                            <i class="ri-bookmark-fill text-white group-hover:text-[#FFCA42] text-xl transition-all duration-700"></i>
-                        </div>
-                    </div>
-                </div>
-                <!-- Card Item -->
-                <div class="group card-item shadow-sm shadow-transparent bg-[#FFCA42] hover:bg-[#1B3764] w-[270px] h-[380px] pt-3 rounded-lg pl-3 pr-3 transition-all duration-700">
-                    <a class="card-img">
-                        <img src="" alt="" class="h-[170px] rounded-md bg-[#1B3764] group-hover:bg-[#FFCA42]">
-                    </a>
-                    <div class="text-card">
-                        <h3 class="mt-[9px] text-white group-hover:text-white text-[18px] transition-all duration-700" style="font-family: 'Quicksand', sans-serif; font-weight: 600;">Si Kancil Yang Cerdik</h3>
-                        <p class="desc text-[12px] text-white mt-3 transition-all duration-700">Diceritakan bahwa kancil seekor hewan kecil yang hidup di hutan belantara. Ia dikenal memiliki akal yang cerdik. Kecerdikannya membuatnya mampu bertahan hidup di hutan.</p>
-                    </div>
-                    <div class="card-btn flex gap-[30px]">
-                        <div>
-                            <button class="btn mt-5 text-white bg-[#1B3764]  group-hover:bg-[#FFCA42] rounded-full w-[144px] h-[30px] transition-all duration-700">Read More</button>
-                        </div>
-                        <div class="mt-[23px] flex gap-3">
-                            <i class="ri-heart-3-fill text-white  group-hover:text-[#FFCA42] text-xl transition-all duration-700"></i>
-                            <i class="ri-bookmark-fill text-white group-hover:text-[#FFCA42] text-xl transition-all duration-700"></i>
-                        </div>
-                    </div>
-                </div>
-                <!-- Card Item -->
-                <div class="group card-item shadow-sm shadow-transparent bg-[#FFCA42] hover:bg-[#1B3764] w-[270px] h-[380px] pt-3 rounded-lg pl-3 pr-3 transition-all duration-700">
-                    <a class="card-img">
-                        <img src="" alt="" class="h-[170px] rounded-md bg-[#1B3764] group-hover:bg-[#FFCA42]">
-                    </a>
-                    <div class="text-card">
-                        <h3 class="mt-[9px] text-white group-hover:text-white text-[18px] transition-all duration-700" style="font-family: 'Quicksand', sans-serif; font-weight: 600;">Si Kancil Yang Cerdik</h3>
-                        <p class="desc text-[12px] text-white mt-3 transition-all duration-700">Diceritakan bahwa kancil seekor hewan kecil yang hidup di hutan belantara. Ia dikenal memiliki akal yang cerdik. Kecerdikannya membuatnya mampu bertahan hidup di hutan.</p>
-                    </div>
-                    <div class="card-btn flex gap-[30px]">
-                        <div>
-                            <button class="btn mt-5 text-white bg-[#1B3764]  group-hover:bg-[#FFCA42] rounded-full w-[144px] h-[30px] transition-all duration-700">Read More</button>
-                        </div>
-                        <div class="mt-[23px] flex gap-3">
-                            <i class="ri-heart-3-fill text-white  group-hover:text-[#FFCA42] text-xl transition-all duration-700"></i>
-                            <i class="ri-bookmark-fill text-white group-hover:text-[#FFCA42] text-xl transition-all duration-700"></i>
-                        </div>
-                    </div>
-                </div>
-                
-                
-
-            </div>
-            <!-- End -->
         </div>
+        @else
+        <h3>No new stories available.</h3>
+        @endif
     </section>
 
     <x-footer>
